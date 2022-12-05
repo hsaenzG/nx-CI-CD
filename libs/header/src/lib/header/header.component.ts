2069bfcf012ed3e4d606f6657d96a33709cbfea2
Hazel Saenz <62771061+hsaenzGCognits@users.noreply.github.com>
2022-12-05 16:28:00 -0600

    update files

---
+++ libs/header/src/lib/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fe-workspace-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {}
